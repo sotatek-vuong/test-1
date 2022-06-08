a32e4c483eaef02a5e2d6a400ec69946f082dc48
sotatek-vuong <hoang.vuong@sotatek.com>
2022-06-08 23:21:11 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { GeistProvider, CssBaseline, Themes } from '@geist-ui/core';
+import '@fontsource/space-grotesk';
+
+const theme = Themes.createFromDark({
+  type: 'my-dark',
+  font: { mono: 'Space Grotesk', prism: 'Space Grotesk', sans: 'Space Grotesk' },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GeistProvider themes={[theme]} themeType="my-dark">
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
 }
-
-export default MyApp
+export default MyApp;
